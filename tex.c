1efcdf79264ea8f26a55cac027844157afea9d8f
Suryanshu <vsuryanshu14@gmail.com>
2026-04-05 18:23:55 +0000

    Add : Error Handling and printing error messeges

--- tex.c
+++ tex.c
@@ -1,40 +1,59 @@
+// Library Includes //
+
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <errno.h>
 #include <termios.h>
 
+// Data //
 
 struct termios orig_termios;
+
+// Terminal //
+
+void die(const char *s) {
+  perror(s);
+  exit(1);
+}
+
 void RawmodeDisable() {
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    die("tcsetattr");
 }
 
 void RawmodeEnable(){
     {
-    tcgetattr(STDIN_FILENO, &orig_termios);
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
     atexit(RawmodeDisable);
 
     struct termios rawmode = orig_termios;
-    rawmode.c_iflag &= ~(IXON); // Disable Ctrl + S and Q
-    rawmode.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG); // disable Ctrl + C, Z and V signals to prevent program kill
 
+    rawmode.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON); // Disable Ctrl + S and Q , Ctrl + M
+    rawmode.c_oflag &= ~(OPOST);
+    rawmode.c_cflag |= (CS8);
+    rawmode.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG); // disable Ctrl + C, Z and V signals to prevent program kill
+    rawmode.c_cc[VMIN] = 0;
+    rawmode.c_cc[VTIME] = 1;
+
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &rawmode) == -1) die("tcsetattr");
         tcsetattr(STDIN_FILENO, TCSAFLUSH, &rawmode);
     };
-    
 }
+
+// Init //
 
 int main(){
 
     RawmodeEnable();
-    char c;
-    while ( read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
+    while (1) {
+        char c = '\0';
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
         if (iscntrl(c)){
-            printf("%d\n", c);
+            printf("%d\r\n", c);
         }
-        else{
-            printf("%d ('%i')", c, c);
-        }
+        if(c == 'q') break;
     }
     return 0;
 }
